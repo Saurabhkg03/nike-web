169dc946e13e9b6d72d4ba916a1ec72f0d8b6d1f
Saurabh Gaikwad <96194597+Saurabhkg03@users.noreply.github.com>
2023-01-16 23:17:39 +0530

    Add files via upload

---
+++ Nike Concept Website/src/pages/Desktop1.js
@@ -0,0 +1,414 @@
+import styles from "./Desktop1.module.css";
+
+const Desktop1 = () => {
+  return (
+    <div className={styles.desktop1}>
+      <img
+        className={styles.f8ed6957de2527695d31bca6605524Icon}
+        alt=""
+        src="../1000014590-f8ed6957de2527695d31bca6605524d910-12-2022-14-40-50-1@2x.png"
+      />
+      <div className={styles.desktop1Inner}>
+        <div className={styles.instanceChild} />
+      </div>
+      <img
+        className={styles.a674b843707642671908bc43fb82eaIcon}
+        alt=""
+        src="../1000014591-a674b843707642671908bc43fb82ea1f10-12-2022-14-42-29-1@2x.png"
+      />
+      <img
+        className={styles.e51a875164a8994df6d12d1b991199Icon}
+        alt=""
+        src="../1000014592-e51a875164a8994df6d12d1b9911999a10-12-2022-14-45-21-1@2x.png"
+      />
+      <h1 className={styles.unwrapPossibilitiesWrapper}>
+        <h1 className={styles.unwrapPossibilities}>Unwrap Possibilities.</h1>
+      </h1>
+      <h2 className={styles.get20Discount}>Get 20% Discount!</h2>
+      <a className={styles.componentParent}>
+        <div className={styles.rectangleWrapper}>
+          <div className={styles.instanceChild} />
+        </div>
+        <div className={styles.exploreWrapper}>
+          <h3 className={styles.explore}>explore</h3>
+        </div>
+      </a>
+      <div className={styles.instanceParent}>
+        <div className={styles.rectangleContainer}>
+          <div className={styles.instanceChild} />
+        </div>
+        <div className={styles.frameWrapper}>
+          <div className={styles.signInWrapper}>
+            <h2 className={styles.signIn}>Sign in</h2>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameParent}>
+        <h2 className={styles.menWrapper}>
+          <a
+            className={styles.men}
+            href="https://www.nike.com/in/men"
+            target="_blank"
+          >
+            Men
+          </a>
+        </h2>
+        <h2 className={styles.menWrapper}>
+          <a
+            className={styles.men}
+            href="https://www.nike.com/in/women"
+            target="_blank"
+          >
+            Women
+          </a>
+        </h2>
+        <h2 className={styles.menWrapper}>
+          <a
+            className={styles.men}
+            href="https://www.nike.com/in/kids"
+            target="_blank"
+          >
+            Kids
+          </a>
+        </h2>
+      </div>
+      <div className={styles.rectangleParent}>
+        <div className={styles.componentItem} />
+        <div className={styles.componentInner} />
+        <div className={styles.frameContainer}>
+          <div className={styles.searchWrapper}>
+            <h2 className={styles.search}>Search</h2>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rectangleGroup}>
+        <div className={styles.groupChild} />
+        <div className={styles.frameGroup}>
+          <h4 className={styles.findAStoreBecomeAMemberSWrapper}>
+            <div className={styles.findAStoreContainer}>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="https://www.nike.com/in/retail"
+                  target="_blank"
+                >
+                  <span className={styles.findAStore2}>FIND A STORE</span>
+                </a>
+              </p>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="https://www.nike.com/in/register"
+                  target="_blank"
+                >
+                  <span className={styles.findAStore2}>BECOME A MEMBER</span>
+                </a>
+              </p>
+              <p className={styles.findAStore}>SIGN UP FOR EMAIL</p>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="https://www.nike.com/in/#site-feedback"
+                  target="_blank"
+                >
+                  <span className={styles.findAStore2}>SEND US FEEDBACK</span>
+                </a>
+              </p>
+              <p className={styles.studentDiscounts}>
+                <a
+                  className={styles.india}
+                  href="https://www.nike.com/in/help/a/student-discount-gs"
+                  target="_blank"
+                >
+                  <span className={styles.findAStore2}>STUDENT DISCOUNTS</span>
+                </a>
+              </p>
+            </div>
+          </h4>
+          <h4 className={styles.getHelpOrderStatusDeliveryWrapper}>
+            <h4 className={styles.search}>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="https://www.nike.com/in/help/a/student-discount-gs"
+                  target="_blank"
+                >
+                  <span className={styles.findAStore2}>&nbsp;</span>
+                </a>
+              </p>
+              <p className={styles.findAStore}>GET HELP</p>
+              <p className={styles.findAStore}>Order Status</p>
+              <p className={styles.findAStore}>Delivery</p>
+              <p className={styles.findAStore}>Returns</p>
+              <p className={styles.findAStore}>Payment Options</p>
+              <p className={styles.findAStore}>
+                Contact Us On Nike.com Inquiries
+              </p>
+              <p className={styles.studentDiscounts}>
+                Contact Us On All Other Inquiries
+              </p>
+            </h4>
+          </h4>
+          <h4 className={styles.aboutNikeNewsCareersInvestWrapper}>
+            <div className={styles.aboutNikeNewsContainer}>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="http://about.nike.com/"
+                  target="_blank"
+                >
+                  ABOUT NIKE
+                </a>
+              </p>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="http://news.nike.com/"
+                  target="_blank"
+                >
+                  News
+                </a>
+              </p>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="https://jobs.nike.com/"
+                  target="_blank"
+                >
+                  Careers
+                </a>
+              </p>
+              <p className={styles.findAStore}>
+                <a
+                  className={styles.india}
+                  href="http://investors.nike.com/"
+                  target="_blank"
+                >
+                  Investors
+                </a>
+              </p>
+              <p className={styles.studentDiscounts}>
+                <a
+                  className={styles.india}
+                  href="https://www.nike.com/in/sustainability"
+                  target="_blank"
+                >
+                  Sustainability
+                </a>
+              </p>
+            </div>
+          </h4>
+        </div>
+        <h5 className={styles.india2023Container}>
+          <a
+            className={styles.india}
+            href="https://www.nike.com/language_tunnel"
+            target="_blank"
+          >
+            <span className={styles.findAStore2}>India</span>
+          </a>{" "}
+          © 2023 Nike, Inc. All Rights Reserved
+        </h5>
+        <h5 className={styles.guidesTermsOfContainer}>
+          {`Guides   `}
+          <a
+            className={styles.india}
+            href="https://www.eshopworld.com/shoppers/help/retailer/nike/terms-and-conditions-of-sale-en/"
+            target="_blank"
+          >
+            <span className={styles.findAStore2}>{`Terms of Sale   `}</span>
+          </a>
+          <a
+            className={styles.india}
+            href={`https://agreementservice.svs.nike.com/in/en_gb/rest/agreement?agreementType=termsOfUse&uxId=com.nike&country=IN&language=en&requestType=redirect`}
+            target="_blank"
+          >
+            <span className={styles.findAStore2}>{`Terms of Use   `}</span>
+          </a>
+          <a
+            className={styles.india}
+            href={`https://agreementservice.svs.nike.com/sg/en_gb/rest/agreement?agreementType=privacyPolicy&uxId=com.nike.unite&country=SG&language=en&requestType=redirect`}
+            target="_blank"
+          >
+            <span className={styles.findAStore2}>Nike Privacy Policy</span>
+          </a>
+        </h5>
+      </div>
+      <div className={styles.groupParent}>
+        <div className={styles.instanceGroup}>
+          <div className={styles.rectangleFrame}>
+            <div className={styles.instanceChild} />
+          </div>
+          <img
+            className={styles.c2f8790f7035467c88a41e9618Icon}
+            alt=""
+            src="../c2f8790f7035467c88a41e961857047e@2x.png"
+          />
+          <a
+            className={styles.nikeAirMaxContainer}
+            href="https://www.nike.com/in/t/air-max-90-ltr-older-shoes-M0TsXt/DV3607-101"
+            target="_blank"
+          >
+            <p className={styles.nikeAirMax}>Nike Air Max 90 LTR</p>
+            <p className={styles.nikeAirMax}>Older Kids' Shoes</p>
+            <p className={styles.nikeAirMax}>1 Colour</p>
+            <p className={styles.studentDiscounts}>MRP : 7 495.00</p>
+          </a>
+        </div>
+        <div className={styles.instanceContainer}>
+          <div className={styles.rectangleDiv}>
+            <div className={styles.instanceChild} />
+          </div>
+          <img
+            className={styles.d890B837475fA1eb850d09e7bdIcon}
+            alt=""
+            src="../0347d890b837475fa1eb850d09e7bd28-1@2x.png"
+          />
+          <a
+            className={styles.nikeAirForceContainer}
+            href="https://www.nike.com/in/t/air-force-1-07-shoes-x9rqBh/FD4205-161"
+            target="_blank"
+          >
+            <p className={styles.nikeAirMax}>Nike Air Force 1 '07 Premium</p>
+            <p className={styles.nikeAirMax}>Men's Shoes</p>
+            <p className={styles.nikeAirMax}>1 Colour</p>
+            <p className={styles.studentDiscounts}>MRP : 10 795.00</p>
+          </a>
+        </div>
+        <div className={styles.instanceGroup}>
+          <div className={styles.rectangleFrame}>
+            <div className={styles.instanceChild} />
+          </div>
+          <img
+            className={styles.bad038c83684a36B632D7984a9Icon}
+            alt=""
+            src="../6bad038c83684a36b632d7984a93ec3d@2x.png"
+          />
+          <a
+            className={styles.nikeAirMoreContainer}
+            href="https://www.nike.com/in/t/air-more-uptempo-96-shoes-KnVxmV/DZ4838-100"
+            target="_blank"
+          >
+            <p className={styles.nikeAirMax}>Nike Air More Uptempo 96</p>
+            <p className={styles.nikeAirMax}>Men's Shoes</p>
+            <p className={styles.nikeAirMax}>1 Colour</p>
+            <p className={styles.studentDiscounts}>MRP : 15 995.00</p>
+          </a>
+        </div>
+        <div className={styles.instanceGroup}>
+          <div className={styles.rectangleFrame}>
+            <div className={styles.instanceChild} />
+          </div>
+          <img
+            className={styles.f6e3bd8568b46e5Bf45Bb4f8fdIcon}
+            alt=""
+            src="../7f6e3bd8568b46e5bf45bb4f8fd07e4a@2x.png"
+          />
+          <a
+            className={styles.nikeAirZoomContainer}
+            href="https://www.nike.com/in/t/air-zoom-tempo-next-road-running-shoes-chNfdw/CI9923-105"
+            target="_blank"
+          >
+            <p className={styles.nikeAirMax}>Nike Air Zoom Tempo NEXT%</p>
+            <p className={styles.nikeAirMax}>Men's Road Running Shoes</p>
+            <p className={styles.nikeAirMax}>4 Colours</p>
+            <p className={styles.studentDiscounts}>MRP : 17 595.00</p>
+          </a>
+        </div>
+        <div className={styles.instanceGroup}>
+          <div className={styles.rectangleFrame}>
+            <div className={styles.instanceChild} />
+          </div>
+          <img
+            className={styles.e84d7e3d2b5e460eBda3Cb8a1aIcon}
+            alt=""
+            src="../e84d7e3d2b5e460ebda3cb8a1ac2a6e9@2x.png"
+          />
+          <a
+            className={styles.nikeInfinityReactContainer}
+            href="https://www.nike.com/in/t/infinity-react-3-road-running-shoes-3Vn30D/DZ3016-600"
+            target="_blank"
+          >
+            <p className={styles.nikeAirMax}>Nike Infinity React 3</p>
+            <p className={styles.nikeAirMax}>Women's Road Running Shoes</p>
+            <p className={styles.nikeAirMax}>2 Colours</p>
+            <p className={styles.studentDiscounts}>MRP : 13 995.00</p>
+          </a>
+        </div>
+        <div className={styles.instanceGroup}>
+          <div className={styles.rectangleFrame}>
+            <div className={styles.rectangleFrame}>
+              <div className={styles.instanceChild} />
+            </div>
+            <img
+              className={styles.airMaxFlyknitRacerShoesRvIcon}
+              alt=""
+              src="../airmaxflyknitracershoesrvntnl-1@2x.png"
+            />
+            <a
+              className={styles.nikeAirMaxContainer1}
+              href="https://www.nike.com/in/t/air-max-flyknit-racer-shoes-rVNTNl/DM9073-002"
+              target="_blank"
+            >
+              <p className={styles.nikeAirMax}>Nike Air Max 90 LTR</p>
+              <p className={styles.nikeAirMax}>Older Kids' Shoes</p>
+              <p className={styles.nikeAirMax}>1 Colour</p>
+              <p className={styles.studentDiscounts}>MRP : 7 495.00</p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <h2 className={styles.discoverOurLatest}>Discover Our Latest Drops -</h2>
+      <img className={styles.desktop1Child} alt="" src="../line-1.svg" />
+      <img
+        className={styles.screenshot202301152038561}
+        alt=""
+        src="../screenshot-20230115-203856-1@2x.png"
+      />
+      <h2 className={styles.comingSoonThis}>Coming Soon This Summer -</h2>
+      <img className={styles.desktop1Item} alt="" src="../line-1.svg" />
+      <div className={styles.desktop1Inner1}>
+        <div className={styles.instanceChild} />
+      </div>
+      <h2 className={styles.nikeAirTechnologyContainer}>
+        <p className={styles.nikeAirMax}>
+          <span className={styles.nikeAirTechnology}>
+            Nike Air technology consists of pressurised air inside a tough yet
+            flexible bag, and provides more flexibility and spring without
+            compromising structure. The Air-Sole units maintain their given form
+            with elasticity, reduce impact and keep the shoe snug and
+            lightweight.
+          </span>
+        </p>
+        <p className={styles.nikeAirMax}>
+          <span className={styles.nikeAirTechnology}>&nbsp;</span>
+        </p>
+        <p className={styles.nikeAirMax}>
+          <span>
+            <span className={styles.span}>{` `}</span>
+            <span>{`   `}</span>
+          </span>
+          <a
+            className={styles.india}
+            href="https://www.nike.com/in/w/air-max-shoes-a6d8hzy7ok"
+            target="_blank"
+          >
+            <span>
+              <span>
+                <span className={styles.findAStore2}>Shop Now</span>
+              </span>
+            </span>
+          </a>
+        </p>
+      </h2>
+      <img className={styles.image4Icon} alt="" src="../image-4@2x.png" />
+      <img className={styles.image5Icon} alt="" src="../image-5@2x.png" />
+      <img className={styles.image6Icon} alt="" src="../image-6@2x.png" />
+      <img className={styles.image7Icon} alt="" src="../image-7@2x.png" />
+      <img className={styles.lineIcon} alt="" src="../line-1.svg" />
+      <h2 className={styles.whatIsNike}> What Is Nike Air?</h2>
+    </div>
+  );
+};
+
+export default Desktop1;
